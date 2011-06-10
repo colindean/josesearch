55587b644a47605253018d7bb0da231147421dfc
Colin Dean <cad@cad.cx>
2011-06-09 21:16:09 -0400

    basic search resulting in single result start

--- josesearch.rb
+++ josesearch.rb
@@ -12,7 +12,46 @@ class JoseSearch
 
   get '/suggestions.json' do 
     content_type :json
-    ["brochures","pamphlets","web sites"].to_json
+    works(params['term']).collect{ |w| w[:work] }.to_json
   end
+
+  get '/search' do
+    q = params['q']
+    list = works(q)
+
+    if list.count == 1
+      if params['pjax'] == 'true'
+        content_type :json 
+        list.first.to_json
+      else
+        redirect list.first[1]
+      end
+    else
+      if params['pjax'] == 'true'
+        content_type :json
+        list.to_json
+      else
+        haml :search, :locals => {:list => list}
+      end
+    end
+  end
+
+  get '/works/brochures' do
+    haml :brochures, :locals => {:type => "brochures"}
+  end
+
+
+  def works(work=nil)
+    works = [ 
+              { :work => "brochures", :url => "/works/brochures" },
+              { :work =>"pamphlets", :url => "/works/pamphlets"},
+              { :work =>"web sites", :url => "/works/websites"},
+              { :work => "videos", :url => "/works/videos"}
+    ]
+    return works if !work
+    return works.select{ |w| w[:work].index work}
+  end
+
+    
 
 end
--- public/js/josesearch.js
+++ public/js/josesearch.js
@@ -4,4 +4,42 @@ $(function(){
     source: "/suggestions.json"
   });
 
+  $('form.query').submit(function(e){
+    e.preventDefault();
+    $.ajax({
+      url: '/search',
+      data: { q: $('input.query').val(), pjax: true },
+      dataType: 'json',
+      success: function(data, textStatus, jqXHR){
+        extData = data;
+        if(data.length){
+          handleMultipleSearchResults(data);
+        }else{
+          retrieveAndShow(data);
+        }
+      }
+    });
+    
+  });
+
 });
+
+function handleMultipleSearchResults(data){
+  $.each(data, function(i,v){
+    addWork(v.work, v.url);
+  });
+}
+
+function addWork(work, url){
+  alert(work + ": " + url);
+}
+
+function retrieveAndShow(wo){
+  $('#search').removeClass('centered');
+  $('#search').addClass('stowed');
+  $('body').append('<section class="content"/>');
+
+  setTimeout("$('section.content').load('"+wo.url+"')", 2000);
+}
+
+var extData;
